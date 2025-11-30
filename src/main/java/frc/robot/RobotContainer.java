51a1696803d637e4927b6917d8d1de91d221acfa
Ehren von Ibsch <ehrenwolf42@icloud.com>
2025-11-30 13:59:24 -0800

    Implement nudge functionality using the d-pad

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,10 +20,31 @@ public class RobotContainer {
     private void configureBindings() {
         swerveSubsystem.setDefaultCommand(driveCommand);
 
-        ChassisSpeeds driverNudgeUp = new ChassisSpeeds(0.25, 0.25, 0.25);
+        ChassisSpeeds driverNudgeUp = new ChassisSpeeds(0.25, 0, 0);
         driverController.povUp().whileTrue(
             Commands.run(() -> {
                 swerveSubsystem.drive(driverNudgeUp);
+            })
+        );
+
+        ChassisSpeeds driverNudgeDown = new ChassisSpeeds(-0.25, 0, 0);
+        driverController.povDown().whileTrue(
+            Commands.run(() -> {
+                swerveSubsystem.drive(driverNudgeDown);
+            })
+        );
+
+        ChassisSpeeds driverNudgeLeft = new ChassisSpeeds(0, 0.25, 0);
+        driverController.povLeft().whileTrue(
+            Commands.run(() -> {
+                swerveSubsystem.drive(driverNudgeLeft);
+            })
+        );
+
+        ChassisSpeeds driverNudgeRight = new ChassisSpeeds(0, -0.25, 0);
+        driverController.povRight().whileTrue(
+            Commands.run(() -> {
+                swerveSubsystem.drive(driverNudgeRight);
             })
         );
     }
